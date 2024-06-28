2d94a3ac6a23849fc614727ec78c4593ea328cc7
iamdramaticleo <iamdramaticleo@gmail.com>
2024-06-28 15:08:35 +0300

    Add shaders subdirectory, modify vertex buffer code
    A shaders directory was added to the graphics module. In related updates, the vertex buffer code saw its vector vertices replaced with equivalent instances of the vec3 data type from the math library. Lastly, a shaders subdirectory was newly introduced in the CMakeLists.txt file.

--- code/engine/graphics/shaders/main.cpp
+++ code/engine/graphics/shaders/main.cpp
@@ -15,7 +15,7 @@ int main()
     auto& platform = core::PlatformInstance::instance();
     auto& window   = core::WindowInstance::instance();
 
-    const base::window_config config { "vertex buffer", { 1024, 768 } };
+    const base::window_config config { "shaders", { 1024, 768 } };
     const auto factory = core::PlatformFactory::create_factory();
 
     platform.init(factory);
