6292c4238ded35e04046b3bf95742b3422a03c5a
iamdramaticleo <iamdramaticleo@gmail.com>
2024-06-29 20:54:42 +0300

    Remove unused GL functions and definitions
    The commit involves deleting unused OpenGL (GL) functions and definitions from glad.c and glad.h files in the libraries/engine/libraries/glad/ directory. Specifically, the code related to GetProgramiv, GetProgramInfoLog, and GL_LINK_STATUS has been removed, as they were not being used in the application.

--- code/engine/graphics/README.md
+++ code/engine/graphics/README.md
@@ -2,3 +2,4 @@
 |:---------------------------------------------------:|
 |    <img src="./opengl/preview.png" width="500">     |
 | <img src="./vertex_buffer/preview.png" width="500"> |
+|    <img src="./shaders/preview.png" width="500">    |
--- code/engine/graphics/shaders/main.cpp
+++ code/engine/graphics/shaders/main.cpp
@@ -6,9 +6,12 @@
 #include "opengl/pipeline.hpp"
 #include "opengl/vertex_array.hpp"
 #include "opengl/buffer.hpp"
+#include "opengl/shader.hpp"
 #include "opengl/macros.hpp"
 
 #include "math/vec3.hpp"
+
+#include "file.hpp"
 
 int main()
 {
@@ -22,6 +25,26 @@ int main()
     window.create(factory, config);
 
     gl::Context::load();
+
+    const auto vert_stage_data = core::File::read("diffuse_vert.spv", std::ios::binary);
+    const auto frag_stage_data = core::File::read("diffuse_frag.spv", std::ios::binary);
+
+    gl::ShaderStage vert_stage { gl::VERTEX_SHADER };
+    vert_stage.create();
+    vert_stage.source(vert_stage_data);
+
+    gl::ShaderStage frag_stage { gl::FRAGMENT_SHADER };
+    frag_stage.create();
+    frag_stage.source(frag_stage_data);
+
+    gl::Shader shader;
+    shader.create();
+    shader.attach(vert_stage);
+    shader.attach(frag_stage);
+    shader.link();
+
+    vert_stage.destroy();
+    frag_stage.destroy();
 
     const std::vector<math::vec3> vertices
     {
@@ -48,6 +71,8 @@ int main()
         gl::Commands::clear(0.5f, 0.5f, 0.5f);
         gl::Commands::clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
 
+        shader.bind();
+
         vertex_array.bind();
         gl::Commands::draw_arrays(gl::TRIANGLES, vertices.size());
 
@@ -57,6 +82,8 @@ int main()
 
     vertices_buffer.destroy();
     vertex_array.destroy();
+
+    shader.destroy();
 
     window.destroy();
     platform.release();
