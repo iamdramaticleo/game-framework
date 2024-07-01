a484024a8abf5af14e39ddf9c4d3da3027854930
iamdramaticleo <iamdramaticleo@gmail.com>
2024-07-01 21:53:58 +0300

    Merge OpenGL implementation to window engine
    The OpenGL implementation previously within its own directory has been merged with the window engine. The graphics/OpenGL project and main.cpp were removed, and their necessary contents were added directly into the window/main.cpp. Some minor changes to the project dependencies were made where necessary.

--- code/engine/graphics/README.md
+++ code/engine/graphics/README.md
@@ -1,6 +1,5 @@
 | Preview                                             |
 |:----------------------------------------------------|
-| <img src="./opengl/preview.png" width="500">        |
 | <img src="./vertex_buffer/preview.png" width="500"> |
 | <img src="./shaders/preview.png" width="500">       |
 | <img src="./index_buffer/preview.png" width="500">  |
--- code/engine/platform/window/main.cpp
+++ code/engine/platform/window/main.cpp
@@ -1,6 +1,11 @@
 #include "window_instance.hpp"
 #include "platform_instance.hpp"
 
+#include "opengl/context.hpp"
+#include "opengl/commands.hpp"
+#include "opengl/pipeline.hpp"
+#include "opengl/macros.hpp"
+
 int main()
 {
     auto& platform = core::PlatformInstance::instance();
@@ -12,8 +17,15 @@ int main()
     platform.init(factory);
     window.create(factory, config);
 
+    gl::Context::load();
+
+    gl::Pipeline::enable(gl::DEPTH_TEST);
+
     while (window.is_active())
     {
+        gl::Commands::clear(0.5f, 0.5f, 0.5f);
+        gl::Commands::clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+
         window.update();
         platform.update();
     }
